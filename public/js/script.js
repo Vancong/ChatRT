e3612e8e16ae04288d8521d54e994c6fb5dc66ee
Tran Van Cong <aanh7108@gmail.com>
2024-08-25 20:00:14 +0700

    done

---
+++ public/js/script.js
@@ -0,0 +1,27 @@
+var socket = io();
+
+// Cập nhật số lượng sản phẩm trong giỏ hàng
+const listInputQuantity = document.querySelectorAll("[cart] input[name='quantity']");
+if (listInputQuantity.length > 0) {
+  listInputQuantity.forEach(input => {
+    input.addEventListener("change", () => {
+      const productId = input.getAttribute("product-id");
+      const quantity = parseInt(input.value);
+
+      if (productId && quantity > 0) {
+        window.location.href = `/cart/update/${productId}/${quantity}`;
+      }
+    })
+  })
+}
+const showAlert = document.querySelector("[show-alert]");
+if (showAlert) {
+  let time = showAlert.getAttribute("show-alert") || 3000;
+  time = parseInt(time);
+
+  setTimeout(() => {
+    showAlert.classList.add("hidden");
+  }, time);
+}
+// End s
+// Hết Cập nhật số lượng sản phẩm trong giỏ hàng
